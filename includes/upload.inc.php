06cbad789fdf15bd6f7421fa9fdd56a1b69c8883
Alex <alexandra.szewc@gmail.com>
2020-12-13 20:00:09 -0500

    read csv using fgetcsv command; fixed new row delimiter issue

--- includes/upload.inc.php
+++ includes/upload.inc.php
@@ -20,14 +20,13 @@ if ( isset($_POST["submit"]) ) {
         echo $_FILES["file"]["name"] . " already exists. ";
       }
       else {
-        //Store file in directory "uploads" with the name of "uploaded_file.txt"
-        $storagename = "uploaded_file.txt";
-        //move_uploaded_file($_FILES["file"]["tmp_name"], $target_dir . $storagename);
-        //echo "Stored in: " . $target_dir . $_FILES["file"]["name"] . "<br/>";
-
         $target_file = $target_dir . basename($_FILES["file"]["name"]);
-        if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
-          echo "The file ". htmlspecialchars(basename( $_FILES["file"]["name"])) . " has been uploaded.";
+        $fileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        if($fileType != "csv") {
+          echo "Sorry, only CSV files are allowed.";
+        }
+        else if (move_uploaded_file($_FILES["file"]["tmp_name"], $target_file)) {
+          echo "The file ". htmlspecialchars(basename( $_FILES["file"]["name"])) . " has been uploaded.<br />";
         }
         else {
           echo "Sorry, there was an error uploading your file.";
@@ -40,48 +39,20 @@ if ( isset($_POST["submit"]) ) {
   }
 }
 
-if (isset($storagename) && $file = fopen($target_dir . $storagename , r ) ) {
+ini_set('auto_detect_line_endings', true);
+if (isset($target_file) && ($file = fopen($target_file , 'r' )) !== FALSE) {
 
   echo "File opened.<br />";
-
-  $firstline = fgets ($file, 4096 );
-      //Gets the number of fields, in CSV-files the names of the fields are mostly given in the first line
-  $num = strlen($firstline) - strlen(str_replace(";", "", $firstline));
-
-      //save the different fields of the firstline in an array called fields
-  $fields = array();
-  $fields = explode( ";", $firstline, ($num+1) );
-
-  $line = array();
-  $i = 0;
-
-      //CSV: one line is one record and the cells/fields are seperated by ";"
-      //so $dsatz is an two dimensional array saving the records like this: $dsatz[number of record][number of cell]
-  while ( $line[$i] = fgets ($file, 4096) ) {
-
-      $dsatz[$i] = array();
-      $dsatz[$i] = explode( ";", $line[$i], ($num+1) );
-
-      $i++;
+  $row = 1;
+  while (($data = fgetcsv($file, 1000, ",")) !== FALSE) {
+    $num = count($data);
+    echo "<p> $num fields in line $row: <br /></p>\n";
+    $row++;
+    for ($c=0; $c < $num; $c++) {
+        echo $data[$c] . "<br />\n";
+    }
   }
-
-      echo "<table>";
-      echo "<tr>";
-  for ( $k = 0; $k != ($num+1); $k++ ) {
-      echo "<td>" . $fields[$k] . "</td>";
-  }
-      echo "</tr>";
-
-  foreach ($dsatz as $key => $number) {
-              //new table row for every record
-      echo "<tr>";
-      foreach ($number as $k => $content) {
-                      //new table cell for every field of the record
-          echo "<td>" . $content . "</td>";
-      }
-  }
-
-  echo "</table>";
+  fclose($file);
 }
 
 /*$target_dir = "../uploads/";
@@ -110,12 +81,6 @@ if (file_exists($target_file)) {
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 500000) {
   echo "Sorry, your file is too large.";
-  $uploadOk = 0;
-}
-
-// Allow certain file formats
-if($fileType != "csv") {
-  echo "Sorry, only CSV files are allowed.";
   $uploadOk = 0;
 }
 
